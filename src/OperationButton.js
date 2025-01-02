719ec208050e569b07b48daeb68b23bc7284cdf8
abinasca <abinas.bagmo@gmail.com>
2025-01-02 12:12:38 +0530

    remoced space in operations button

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -3,10 +3,5 @@ export default function OperationButton({ dispatch, operation }) {
     return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}
     >
         {operation}
-
-
-
-
-        
     </button>
 }
