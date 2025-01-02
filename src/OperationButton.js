b11c7cbe77a3d8cce88a7a244df346b9a784a353
abinasca <abinas.bagmo@gmail.com>
2025-01-02 11:56:30 +0530

    space in operation button is file

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -3,5 +3,10 @@ export default function OperationButton({ dispatch, operation }) {
     return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}
     >
         {operation}
+
+
+
+
+        
     </button>
 }
