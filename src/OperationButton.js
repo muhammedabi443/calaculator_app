67f2760b3500e448f2c584de2a11520db4b64121
abinasca <abinas.bagmo@gmail.com>
2025-01-03 10:00:37 +0530

    bvfb

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -2,6 +2,6 @@ import { ACTIONS } from './App'
 export default function OperationButton({ dispatch, operation }) {
     return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}
     >
-this is for exmple
-    </button>
+        {operation}
+        </button>
 }
